6842d48635e935a7decb1443c9006473c35d3093
Elena-Weber <crochetmiracles@gmail.com>
2021-06-07 17:22:01 -0400

    commit tree 3319a889260f7e4ee402ba6dc67aa90e4e2e5d0e

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+    let allNegative = sourceArray.map(num => num * -1);
+    return allNegative;
+}
+function mapToNoChange(sourceArray) {
+    let noChange = [];
+    for (let num of sourceArray) {
+        noChange.push(num);
+    }
+    return noChange;
+}
+function mapToDouble(sourceArray) {
+    let multiplied = sourceArray.map(num => num * 2);
+    return multiplied;
+}
+function mapToSquare(sourceArray) {
+    let squared = [];
+    for (let num of sourceArray) {
+        squared.push(num * num);
+    }
+    return squared;
+}
+function reduceToTotal(sourceArray, startingPoint=0) {
+    const reducer = (accumulator, item) => {
+        return accumulator + item;
+    };
+    let total = sourceArray.reduce(reducer, startingPoint);
+    return total;
+}
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (!sourceArray[i]) return false
+    }
+    return true
+}
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (sourceArray[i]) return true
+    }
+    return false
+}
